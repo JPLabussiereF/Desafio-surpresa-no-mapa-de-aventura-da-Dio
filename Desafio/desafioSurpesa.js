ad1b7fcf8cd267659f29a4dc2797fbcceeda2b3a
JPLabussiereF <labussework@gmail.com>
2024-02-21 01:34:32 -0300

    Desafio Finalizado

--- Desafio/desafioSurpesa.js
+++ Desafio/desafioSurpesa.js
@@ -1,18 +1,23 @@
-// let nomesP = ["Poochyena", "Zigzagoon", "Dragonite", "Dragonite", "Dragonite", "Pochyena", "Wurmple"];
 
-let pokemons = [["Poochyena", "M", 2, "tem 13 pontos de vida!"],
-                ["Zigzagoon", "F", 2, "tem 13 pontos de vida!"],
-                ["Dragonite", "M", 5, "tem 25 pontos de vida!"],
-                ["Dragonite", "F", 5, "tem 24 pontos de vida!"],
-                ["Dragonite", "F", 5, "tem 24 pontos de vida!"],
-                ["Poochyena", "F", 3, "tem 15 pontos de vida!"],
-                ["Wurmple", "M", 2, "tem 14 pontos de vida, mas está apenas com 7 pontos de vida!"]];
+let pokemons = [["Poochyena", "M", 2, "possui 13 de HP!"],
+                ["Zigzagoon", "F", 2, "possui 13 de HP!"],
+                ["Dragonite", "M", 5, "possui 25 de HP!"],
+                ["Dragonite", "F", 5, "possui 24 de HP!"],
+                ["Dragonite", "F", 5, "possui 24 de HP!"],
+                ["Poochyena", "F", 3, "possui 15 de HP!"],
+                ["Wurmple", "M", 2, "possui 14 de HP. Mas, está apenas com 7 de HP!"]];
 
-        // console.log("O pokemom " + pokemons[0][0] + " é do sexo " + pokemons[0][1] + ", atualmente está level " + pokemons[0][2] + " e " + pokemons[0][3])
-    
-        
-        for (let i = 0; i < pokemons.length; i++) { // length é uma propriedade de arrays que retorna o número de elementos na matriz
+        for(let i = 0; i < pokemons.length; i++ ){ // Lembrete: length é uma propriedade de arrays que retorna o número de elementos na matriz
             let pokemon = pokemons[i];
+            let message = "O pokemon " + pokemon[0] + " (" + pokemon[1] + ") " + "foi cadastrado com sucesso!"
+            console.log(message);
+        }
+
+        console.log(" ")
+        console.log("Abaixo fornecemos mais informações sobre os pokemons adiquiridos: ")
+
+        for (let i = 0; i < pokemons.length; i++) { // Lembrete: length é uma propriedade de arrays que retorna o número de elementos na matriz
+            let pokemon = pokemons[i];      // Lembrete²: i++ é um operador de incremento
             let message = "O pokemon " + pokemon[0] + " (" + pokemon[1] + "), de nível " + pokemon[2] + " " + pokemon[3];
             console.log(message);
         }
