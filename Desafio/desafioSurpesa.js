3343eedb9f2ea79eae0a537eba469a0ce8ef4b0b
JPLabussiereF <labussework@gmail.com>
2024-02-21 01:04:36 -0300

    Meu primeiro teste

--- Desafio/desafioSurpesa.js
+++ Desafio/desafioSurpesa.js
@@ -1,2 +1,13 @@
-console.log("teste");
-awfjfajfaifja;
+// let nomesP = ["Poochyena", "Zigzagoon", "Dragonite", "Dragonite", "Dragonite", "Pochyena", "Wurmple"];
+
+let pokemons = [["Poochyena", "M", 2, "tem 3 pontos de vida!"],
+                ["Zigzagoon", "F", 2, "tem 13 pontos de vida!"],
+                ["Dragonite", "M", 5, "tem 25 pontos de vida!"],
+                ["Dragonite", "F", 5, "tem 24 pontos de vida!"],
+                ["Dragonite", "F", 5, "tem 24 pontos de vida!"],
+                ["Poochyena", "F", 3, "tem 15 pontos de vida!"],
+                ["Wurmple", "M", 2, , "tem 14 pontos de vida, mas está apenas com 7 pontos de vida!"]];
+
+        console.log("O pokemom " + pokemons[0][0] + " é do sexo " + pokemons[0][1] + ", atualmente está level " + pokemons[0][2] + " e " + pokemons[0][3])
+
+    
